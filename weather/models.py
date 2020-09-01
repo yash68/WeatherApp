f127ed17a644ccaaa3dd8ab08cb58bfd2f61f50b
yash68 <gargyash512@gmail.com>
2020-09-01 19:35:15 +0530

    Updating temprature change feature

--- weather/models.py
+++ weather/models.py
@@ -3,7 +3,12 @@ from django.db import models
 # Create your models here.
 class City(models.Model):
 	name = models.CharField(max_length = 25)
-	#temprature = models.IntegerField()
-
+	cityid = models.IntegerField(blank = True, null = True)
+	description = models.CharField(max_length = 250,blank = True, null = True )
+	ftemp = models.FloatField(blank = True, null = True)
+	ctemp = models.FloatField(blank = True, null = True)
+	flag = models.IntegerField(default = 0)
+	icon = models.CharField(max_length=50,blank = True, null = True)
+	
 	def __str__(self):
 		return self.name
